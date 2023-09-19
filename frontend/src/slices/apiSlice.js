b238f7e06162275b54b03762669949336d1c5f80
Sanilya <panchalsanilya@gmail.com>
2023-09-19 13:50:29 +0530

    better example

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+// TODO: check api url endpoint from env
+const baseQuery = fetchBaseQuery({ baseUrl: ProcessingInstruction.env.REACT_APP_API_URL });
 
 export const apiSlice = createApi({
   baseQuery,
